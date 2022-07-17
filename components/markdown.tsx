073b3565848715ffab6567a0a19c703be5b649ad
qianxi0410 <894871277@qq.com>
2022-07-17 19:14:06 +0800

    feat: post show page

---
+++ components/markdown.tsx
@@ -0,0 +1,62 @@
+/* eslint-disable jsx-a11y/heading-has-content */
+import type { SpecialComponents } from 'react-markdown/lib/ast-to-react'
+import type { NormalComponents } from 'react-markdown/lib/complex-types'
+import type { ReactMarkdownOptions } from 'react-markdown/lib/react-markdown'
+import { ReactMarkdown } from 'react-markdown/lib/react-markdown'
+import rehypeRaw from 'rehype-raw'
+import remarkGfm from 'remark-gfm'
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
+import { dracula } from 'react-syntax-highlighter/dist/cjs/styles/prism'
+
+const components: Partial<Omit<NormalComponents, keyof SpecialComponents> & SpecialComponents> = {
+  h1: properties => <h1 {...properties} className="text-3xl font-bold my-5" />,
+  h2: properties => <h2 {...properties} className="text-2xl font-bold my-4" />,
+  h3: properties => <h3 {...properties} className="text-xl font-bold my-3" />,
+  h4: properties => <h4 {...properties} className="text-xl font-bold my-3" />,
+  h5: properties => <h5 {...properties} className="text-xl font-bold my-2" />,
+  h6: properties => <h6 {...properties} className="text-xl font-bold my-2" />,
+  p: properties => <p {...properties} className="text-xl my-4" />,
+  a: properties => <a {...properties} className="text-blue-500 hover:text-blue-800 transition-colors" />,
+  strong: properties => <strong {...properties} className="font-bold" />,
+  em: properties => <em {...properties} className="font-italic" />,
+  blockquote: properties => <blockquote {...properties} className="border-l-4 border-gray-300 bg-green-50 py-1 px-10" />,
+  ul: properties => <ul {...properties} className="text-lg pl-5" />,
+  ol: properties => <ol {...properties} className="text-lg list-decimal pl-5" />,
+  li: properties => <li {...properties} className="text-lg my-2" />,
+  table: properties => <table {...properties} className="w-4/5 text-lg border-b-1 bg-gray-50  mx-auto px-2 my-3" />,
+  thead: properties => <thead {...properties} className="bg-gray-100" />,
+  tbody: properties => <tbody {...properties} className="bg-white text-center" />,
+  tr: properties => <tr {...properties} className="border-b-2 border-gray-100" />,
+  del: properties => <del {...properties} className="text-red-500" />,
+  code({ inline, className, children, ...properties }) {
+    const match = /language-(\w+)/.exec(className || '')
+    return (!inline && match)
+      ? (
+        <SyntaxHighlighter
+          showLineNumbers
+          style={dracula as any}
+          language={match[1]}
+          PreTag="div"
+          {...properties}
+        >
+          {String(children).replace(/\n$/, '')}
+        </SyntaxHighlighter>)
+      : (
+        <code className={className} {...properties}>
+          {children}
+        </code>)
+  },
+}
+
+const Markdown: React.FC<ReactMarkdownOptions> = properties => (
+  <ReactMarkdown
+    remarkPlugins={[remarkGfm]}
+    rehypePlugins={[rehypeRaw]}
+    components={
+      components
+    }
+    {...properties}
+  />
+)
+
+export default Markdown
---
+++ pages/post/[id].tsx
@@ -0,0 +1,90 @@
+import type { NextPage } from 'next'
+import Link from 'next/link'
+import Markdown from '../../components/markdown'
+
+const markdown = `
+# im the tittle
+
+## When should I use this?
+
+There are other ways to use markdown in React out there so why use this one?
+The two main reasons are that they often rely on dangerouslySetInnerHTML or
+have bugs with how they handle markdown.
+react-markdown uses a syntax tree to build the virtual dom which allows for
+updating only the changing DOM instead of completely overwriting.
+react-markdown is 100% CommonMark compliant and has plugins to support other
+syntax extensions (such as GFM).
+
+These features are supported because we use [unified][], specifically [remark][]
+for markdown and [rehype][] for HTML, which are popular tools to transform
+content with plugins.
+
+This package focusses on making it easy for beginners to safely use markdown in
+React.
+When you’re familiar with unified, you can use a modern hooks based alternative
+[react-remark][react-remark] or [rehype-react][rehype-react] manually.
+If you instead want to use JavaScript and JSX *inside* markdown files, use
+[MDX][].
+
+### uriTransformer
+
+Our default URL transform, which you can overwrite (see props above).
+It’s given a URL and cleans it, by allowing only http: https:, mailto:,
+and tel: URLs, absolute paths (/example.png), and hashes (#some-place).
+
+See the [source code here][uri].
+
+A paragraph with *emphasis* and **strong importance**.
+
+> A block quote with ~strikethrough~ and a URL: https://reactjs.org.
+
+Lists
+
+* [x] todo
+* [x] done
+
+A table:
+
+|  表头   | 表头  |
+|  ----  | ----  |
+| 单元格  | 单元格 |
+| 单元格  | 单元格 |
+
+wqewqewqe
+\`\`\`go
+func main() {
+  var a = 2   var a = 2   var a = 2  var a = 2  var a = 2   var a = 2
+  b := 3
+
+  fmt.Println(a, b)
+}
+\`\`\`
+213213231
+
+This ~is not~ strikethrough, but ~~this is~~!
+`
+
+const PostDetail: NextPage = () => (
+  <div className="my-20">
+    <div className="flex flex-col items-start mb-10">
+      <div className="text-4xl font-bold">
+      this is the article title
+      </div>
+      <div className="text-xl">
+        <span>q1anx1</span> / <span>2021.05.03</span> / <span>2022.07.03</span>
+      </div>
+    </div>
+
+    <Markdown className="text-2xl font-normal">
+      {markdown}
+    </Markdown>
+
+    <div>
+      <Link href="/blog">
+        <a className="flex flex-row justify-end text-2xl text-gray-500 hover:text-gray-700 transition-colors">cd ..</a>
+      </Link>
+    </div>
+  </div>
+)
+
+export default PostDetail
