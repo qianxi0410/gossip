2e6a70b6185735a4ddc2ddcafcf61db5f5a0789d
qianxi0410 <894871277@qq.com>
2022-07-21 18:23:45 +0800

    chore(404): fix `404` page ui bug

--- pages/404.tsx
+++ pages/404.tsx
@@ -4,10 +4,10 @@ import type { NextPage } from 'next'
 
 const NotFound: NextPage = () => (
 
-  <div className="flex flex-col sm:flex-row sm:mt-56 mt-28 sm:space-x-5 space-y-2">
-    <span className="text-6xl sm:text-8xl text-gray-900">Oops!</span>
+  <div className="flex flex-col xl:flex-row xl:mt-56 mt-28 xl:space-x-5 space-y-2">
+    <span className="text-6xl lg:text-8xl text-gray-900">Oops!</span>
     <br />
-    <div className="self-center text-lg">
+    <div className="xl:self-center text-lg xl:text-xl">
       Seem to have lost your way.
       <br />
       Click <Link href="/"><a className="text-blue-600">here</a></Link> to go back to the home page.
