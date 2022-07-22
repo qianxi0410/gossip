1c9abe7b9ae6ae4d4088defedd423d9d20809998
qianxi0410 <894871277@qq.com>
2022-07-22 10:09:34 +0800

    feat: dark mod

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import '../styles/globals.css'
 
+import { ThemeProvider } from 'next-themes'
 import Head from 'next/head'
 
 import type { AppProps } from 'next/app'
@@ -10,7 +11,9 @@ const MyApp = ({ Component, pageProps }: AppProps) => (
       <title>{process.env.OWNER!}&apos;s website</title>
     </Head>
     <div className="w-full sm:w-10/12 md:w-9/12 lg:w-7/12 container">
-      <Component {...pageProps} />
+      <ThemeProvider>
+        <Component {...pageProps} />
+      </ThemeProvider>
     </div>
   </>
 )
