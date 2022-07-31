95ba5ef1ca3f5b004339df3d42e8b5b7e6e500f5
q1anx1 <55543743+qianxi0410@users.noreply.github.com>
2022-07-31 12:52:54 +0800

    feat: back2top button (#13)

---
+++ components/back2top.tsx
@@ -0,0 +1,32 @@
+import Triangle from './icons/triangle'
+
+import { useEffect, useState } from 'react'
+
+const handleClick = () => {
+  window.scrollTo({ top: 0, behavior: 'smooth' })
+}
+
+const Back2Top = () => {
+  const [show, setShow] = useState(false)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > window.innerHeight) setShow(true)
+      else setShow(false)
+    }
+
+    window.addEventListener('scroll', handleScroll)
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
+
+  return (
+    <div className="bottom-10 right-10 cursor-pointer fixed hidden lg:block" onClick={handleClick}>
+      <Triangle className={`${show ? 'opacity-100' : 'opacity-0'} duration-700 transition-opacity ease-in-out`} />
+    </div>
+  )
+}
+
+export default Back2Top
