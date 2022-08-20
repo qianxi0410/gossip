f30ecf456553bdaec5fd6d998db0c22753f0f452
qianxi0410 <894871277@qq.com>
2022-08-20 10:16:09 +0800

    fix: remove special tag when build pages

---
+++ lib/util.ts
@@ -0,0 +1,24 @@
+import type { Post } from 'gossip'
+
+// format date string
+export const formatDate = (date: string) => {
+  const d = new Date(date)
+  // if year is current year, just return month and day
+  if (d.getFullYear() === new Date().getFullYear()) return `${d.getMonth() + 1}月${d.getDate()}日`
+  return `${d.getFullYear()}年${d.getMonth() + 1}月${d.getDate()}日`
+}
+
+// format post list content
+export const groupByDate = (posts: Post[]) => {
+  const map = new Map<string, Post[]>()
+
+  for (const post of posts) {
+    const year = post.created_at.split('-')[0]
+    const list = map.get(year) || []
+
+    list.push(post)
+    map.set(year, list)
+  }
+
+  return map
+}
